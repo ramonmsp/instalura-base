e66ea432d58c34c10f7a23ba4f067e6039b5d03c
Ramon Maciel <ramon.maciel@liferay.com>
2021-03-17 20:33:54 -0300

    mod2 aula 1

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from '../src/components/commons/Footer';
 import Menu from '../src/components/commons/Menu';
 import Text from '../src/components/foundation/Text';
 import Button from '../src/components/commons/Button';
 import Grid from '../src/components/foundation/layout/Grid';
 import Box from '../src/components/foundation/layout/Box';
+import Modal from '../src/components/commons/Modal';
 
 export default function Home() {
+  const [isModalOpen, setModalState] = useState(false);
+
   return (
     <Box
       flex="1"
@@ -18,6 +21,24 @@ export default function Home() {
       backgroundRepeat="no-repeat"
       backgroundPosition="bottom right"
     >
+
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setModalState(false)}
+      >
+        {
+          (modalProps) => (
+            <Box
+              backgroundColor="white"
+              // eslint-disable-next-line react/jsx-props-no-spreading
+              {...modalProps}
+            >
+              Modal daora
+            </Box>
+          )
+        }
+      </Modal>
+
       <Menu />
 
       <Grid.Container
@@ -72,6 +93,7 @@ export default function Home() {
                 md: 'initial',
               }}
               display="block"
+              onClick={() => setModalState(!isModalOpen)}
             >
               Cadastrar
             </Button>
